cf1145d8dc8dd9f7164fe234f6d434b62d67d568
Vamsi <58515346+vamsichilukuri@users.noreply.github.com>
2020-08-21 17:24:17 +0530

    initial commit

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -5,29 +5,29 @@ User.hasMany(Post);
 User.hasOne(Profile);
 
 //  GET All User's
-const allUsers = (req, res) => {
-    User.findAll({ include: [Post, Profile] })
+const allUsers = async (req, res) => {
+   await User.findAll({ include: [Post, Profile] })
         .then((users) => res.send(users))
         .catch((error) => res.send(error));
 };
 
 //  INSERT New User
-const create = (req, res) => {
-    User.create({ username: req.body.username, age: req.body.age })
+const create = async (req, res) => {
+   await User.create({ username: req.body.username, age: req.body.age })
         .then((user) => res.send(user))
         .catch((error) => res.send(error));
 };
 
 //  GET One User By ID
-const getOne = (req, res) => {
-    User.findByPk(req.params.id)
+const getOne = async (req, res) => {
+   await User.findByPk(req.params.id)
         .then((user) => res.send(user))
         .catch((error) => res.send(error));
 };
 
 // UPDATE User By ID
-const update = (req, res) => {
-    User.update(
+const update = async (req, res) => {
+   await User.update(
         { username: req.body.username, age: req.body.age },
         { where: { id: req.params.id } }
     )
@@ -36,8 +36,8 @@ const update = (req, res) => {
 };
 
 //  DELETE User By ID
-const deleteOne = (req, res) => {
-    User.destroy({ where: { id: req.params.id } })
+const deleteOne = async (req, res) => {
+   await User.destroy({ where: { id: req.params.id } })
         .then(() => res.send("User deleted Successfully"))
         .catch((error) => res.send(error));
 };
